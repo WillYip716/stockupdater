7424ea980cf2b1e43edf7aa62f39b72cb4ec0ece
WillYip716 <william.yip.716@gmail.com>
2020-10-15 17:09:00 -0700

    basic logic for pulling percent change

--- main.js
+++ main.js
@@ -3,15 +3,34 @@ const {getStocks} = require('./stockfinder');
 var cron = require('node-cron');
 let counter = 1;
 
+async function stockChanges(){
+    /*cron.schedule('* * * * *', () => {
+        /* 
+            generate your report here then send the report using 
+            any reportSender logic that you implemnted email, telegram bot,...
+        
+        //sendMail(`hello world ${counter}`, "this is email body it can contain html also");
+        console.log("this is counter: " + counter);
+        counter++;
+    });*/
+    let list = ["pton","tsla","ual","sq"];
+    let dataList = [];
+    for (let i = 0; i < list.length; i++) {
+        let val = await getStocks(list[i]);
+        val.symbol = list[i];
+        dataList.push(val);
+    }
 
-cron.schedule('* * * * *', () => {
-    /* 
-        generate your report here then send the report using 
-        any reportSender logic that you implemnted email, telegram bot,...
-    */
-    //sendMail(`hello world ${counter}`, "this is email body it can contain html also");
-    console.log("this is counter: " + counter);
-    counter++;
-});
+    for(let j = 0; j<dataList.length;j++){
+        let percentChange = 100*(parseFloat(dataList[j].c)-parseFloat(dataList[j].o))/parseFloat(dataList[j].o);
+        console.log(dataList[j].symbol + ": " + percentChange.toFixed(2) +"%"); 
+        console.log("current: " + dataList[j].c); 
+        console.log("open: " + dataList[j].o +  "\n"); 
+    }
+    
+    /*console.log("current: " + data.c);
+    console.log("opening: " + data.o);
+    console.log(percentChange.toFixed(2) + "%"); */
+}
 
-getStocks();
+stockChanges();
--- stockfinder.js
+++ stockfinder.js
@@ -1,12 +1,20 @@
 const axios = require('axios');
 require("dotenv").config();
 
-async function getStocks(){
-    axios.get('https://finnhub.io/api/v1/quote?symbol=AAPL&token='+process.env.FINPW).then(res => {
+async function getStocks(sym){
+    /*axios.get('https://finnhub.io/api/v1/quote?symbol='+sym+'&token='+process.env.FINPW).then(res => {
         console.log(res.data);
+        return res.data;
     }).catch(error => {
         console.log(error);
-    });
+    });*/
+
+    try {
+        let info = await axios.get('https://finnhub.io/api/v1/quote?symbol='+sym.toUpperCase()+'&token='+process.env.FINPW);
+        return info.data;
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 
