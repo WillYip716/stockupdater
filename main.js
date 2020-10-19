9b10a74c37b5fcaf4447d6069fced58f1c18e84f
WillYip716 <william.yip.716@gmail.com>
2020-10-19 15:57:37 -0700

    schedule update and procfile

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-//const {sendMail} = require('./reportsender');
+const {sendMail} = require('./reportsender');
 const {getStocks,getEarningsCalender} = require('./stockfinder');
 var cron = require('node-cron');
-let counter = 1;
 
 async function stockChanges(){
-    /*cron.schedule('* * * * *', () => {
-        /* 
-            generate your report here then send the report using 
-            any reportSender logic that you implemnted email, telegram bot,...
+    cron.schedule('0 14 * * 1-5', async () => {
         
-        //sendMail(`hello world ${counter}`, "this is email body it can contain html also");
-        console.log("this is counter: " + counter);
-        counter++;
-    });*/
-    let list = ["pton","tsla","ual","sq"];
-    let dataList = [];
+        let now = new Date().toISOString().slice(0,10);
+        let list = ["pton","tsla","ual","sq"];
+        let dataList = [];
+        let output = [];
 
-    for (let i = 0; i < list.length; i++) {
-        let val = await getStocks(list[i]);
-        let calender = await getEarningsCalender(list[i]);
-        val.symbol = list[i];
-        val.nextearnings = calender;
-        dataList.push(val);
-    }
+        for (let i = 0; i < list.length; i++) {
+            let val = await getStocks(list[i]);
+            let calender = await getEarningsCalender(list[i]);
+            val.symbol = list[i].toUpperCase();
+            val.nextearnings = calender;
+            dataList.push(val);
+        }
 
-    for(let j = 0; j<dataList.length;j++){
-        let percentChange = 100*(parseFloat(dataList[j].c)-parseFloat(dataList[j].o))/parseFloat(dataList[j].o);
-        console.log(dataList[j].symbol + ": " + percentChange.toFixed(2) +"%"); 
-        console.log("current: " + dataList[j].c); 
-        console.log("open: " + dataList[j].o); 
-        console.log("next earnings date: " + dataList[j].nextearnings +  "\n");
-    }
+        for(let j = 0; j<dataList.length;j++){
+            let percentChange = 100*(parseFloat(dataList[j].c)-parseFloat(dataList[j].o))/parseFloat(dataList[j].o);
+            output.push(dataList[j].symbol + ": " + percentChange.toFixed(2) +"%"); 
+            output.push("current: " + dataList[j].c); 
+            output.push("open: " + dataList[j].o); 
+            output.push("next earnings date: " + dataList[j].nextearnings +  "\n");
+        }
+        
+        sendMail(`Stock Reports ${now}`, "<p style='white-space:pre-wrap;'>" + output.join("\n") + "</p>");
+        console.log("success");
+    });
+    
 }
 
 stockChanges();
